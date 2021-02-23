11e4717fa41745939c2ed4c8898337bec0c80f04
dsenkin <denisx9.0c@gmail.com>
2021-02-23 15:15:31 +0100

    fixes

--- training/psp_encoders.py
+++ training/psp_encoders.py
@@ -15,13 +15,20 @@ class GradualStyleBlock(Module):
         self.spatial = spatial
         num_pools = int(np.log2(spatial))
         modules = []
-        modules += [Conv2d(in_c, out_c, kernel_size=1, stride=1, padding=0),
-                    Conv2d(in_c, out_c, kernel_size=3, stride=2, padding=1, groups=out_c),
-                    nn.LeakyReLU(inplace=True)]
+        modules += [Conv2d(in_c, out_c//2, kernel_size=1, stride=1, padding=0),
+                    Conv2d(out_c//2, out_c//2, kernel_size=3, stride=2, padding=1, groups=out_c//2),
+                    nn.LeakyReLU(inplace=True),
+                    nn.InstanceNorm2d(out_c // 2),
+                    Conv2d(out_c // 2, out_c, kernel_size=1, stride=1, padding=0),
+                    nn.LeakyReLU(inplace=True)
+                    ]
         for i in range(num_pools - 1):
             modules += [
-                Conv2d(out_c, out_c, kernel_size=1, stride=1, padding=0),
-                Conv2d(out_c, out_c, kernel_size=3, stride=2, padding=1, groups=out_c),
+                Conv2d(out_c, out_c//2, kernel_size=1, stride=1, padding=0),
+                Conv2d(out_c//2, out_c//2, kernel_size=3, stride=2, padding=1, groups=out_c//2),
+                nn.LeakyReLU(inplace=True),
+                nn.InstanceNorm2d(out_c//2),
+                Conv2d(out_c // 2, out_c, kernel_size=1, stride=1, padding=0),
                 nn.LeakyReLU(inplace=True)
             ]
         self.convs = nn.Sequential(*modules)
