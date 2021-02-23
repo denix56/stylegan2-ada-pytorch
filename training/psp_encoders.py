02ebcf3bea7e4499e1b047c71fe3ca4eb7bcde9e
dsenkin <denisx9.0c@gmail.com>
2021-02-23 15:10:15 +0100

    fixes

--- training/psp_encoders.py
+++ training/psp_encoders.py
@@ -101,9 +101,9 @@ class GradualStyleEncoder(Module):
         modulelist = list(self.body._modules.values())
         for i, l in enumerate(modulelist):
             x = l(x)
-            if i == 11:
+            if i == 14:
                 c1 = x
-            elif i == 17:
+            elif i == 20:
                 c2 = x
             elif i == 23:
                 c3 = x
