903a8d13f9835ab5ad8c24724c246be2218b1875
dsenkin <denisx9.0c@gmail.com>
2021-03-11 01:29:59 +0100

    fixes

--- training/stylegan_lightning.py
+++ training/stylegan_lightning.py
@@ -70,6 +70,7 @@ class StyleGAN2(pl.LightningModule):
             for param in phase.module.parameters():
                 if param.grad is not None:
                     misc.nan_to_num(param.grad, nan=0, posinf=1e5, neginf=-1e5, out=param.grad)
+            print(optimizer_closure)
             optimizer.step(optimizer_closure)
 
     def on_validation_start(self):
