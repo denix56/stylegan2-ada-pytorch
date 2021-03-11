872c8f44aabcd96209f07063453dbdc0e7926fd1
dsenkin <denisx9.0c@gmail.com>
2021-03-11 01:13:22 +0100

    fixes

--- training/dataloader_lightning.py
+++ training/dataloader_lightning.py
@@ -8,7 +8,7 @@ class StyleGANDataModule(pl.LightningDataModule):
         super().__init__()
         self._batch_size_per_unit = batch_size_per_unit
         self._data_loader_kwargs = data_loader_kwargs
-        self.training_set = dnnlib.util.construct_class_by_name(**self.training_set_kwargs)
+        self.training_set = dnnlib.util.construct_class_by_name(**training_set_kwargs)
 
     @property
     def label_dim(self):
