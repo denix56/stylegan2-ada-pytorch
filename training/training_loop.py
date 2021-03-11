843f512333d43203bf02209c7bb46b591e8e2aea
dsenkin <denisx9.0c@gmail.com>
2021-03-11 01:24:10 +0100

    fixes

--- training/training_loop.py
+++ training/training_loop.py
@@ -193,10 +193,8 @@ def training_loop(
     fid50k = FID(max_real=None, num_gen=50000)
 
     net = StyleGAN2(G=G, D=D, G_opt_kwargs=G_opt_kwargs, D_opt_kwargs=D_opt_kwargs, augment_pipe=augment_pipe,
-                    training_set=training_set, batch_size=batch_gpu, r1_gamma=loss_kwargs.r1_gamma,
-                    pl_batch_shrink=loss_kwargs.pl_batch_shrink, pl_decay=loss_kwargs.pl_decay,
-                    pl_weight=loss_kwargs.pl_weight, G_reg_interval=G_reg_interval, D_reg_interval=D_reg_interval,
-                    ema_kimg=ema_kimg, ema_rampup=ema_rampup, metrics=[fid50k])
+                    training_set=training_set, batch_size=batch_gpu, G_reg_interval=G_reg_interval, D_reg_interval=D_reg_interval,
+                    ema_kimg=ema_kimg, ema_rampup=ema_rampup, metrics=[fid50k], **loss_kwargs)
 
     trainer = pl.Trainer(gpus=num_gpus, accelerator='ddp', fast_dev_run=True,
                          benchmark=cudnn_benchmark, max_steps=total_kimg//(batch_size*num_gpus)*1000)
