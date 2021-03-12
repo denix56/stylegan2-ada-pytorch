029d150820f4b5858ce2a3ed3016de82ab31c3d8
dsenkin <denisx9.0c@gmail.com>
2021-03-12 02:54:47 +0100

    fixes

--- training/stylegan_lightning.py
+++ training/stylegan_lightning.py
@@ -77,11 +77,11 @@ class StyleGAN2(pl.LightningModule):
     def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_idx, optimizer_closure,
                        on_tpu, using_native_amp, using_lbfgs):
         phase = self.phases[optimizer_idx]
-        # if batch_idx % phase.interval == 0:
-        #     for param in phase.module.parameters():
-        #         if param.grad is not None:
-        #             misc.nan_to_num(param.grad, nan=0, posinf=1e5, neginf=-1e5, out=param.grad)
-        optimizer.step(closure=optimizer_closure)
+        if batch_idx % phase.interval == 0:
+            for param in phase.module.parameters():
+                if param.grad is not None:
+                    misc.nan_to_num(param.grad, nan=0, posinf=1e5, neginf=-1e5, out=param.grad)
+            optimizer.step(closure=optimizer_closure)
 
     # def on_validation_start(self):
     #     if self.metrics:
