0a086876697f45618a7b172af2c58d96c5e9743c
dsenkin <denisx9.0c@gmail.com>
2021-03-14 17:08:46 +0100

    fixes

--- training/stylegan_lightning.py
+++ training/stylegan_lightning.py
@@ -74,7 +74,18 @@ class StyleGAN2(pl.LightningModule):
                 if param.grad is not None:
                     misc.nan_to_num(param.grad, nan=0, posinf=1e5, neginf=-1e5, out=param.grad)
         optimizer.step(closure=optimizer_closure)
-        optimizer.zero_grad()
+
+        if batch_idx > 7:
+            state_dict_old = torch.load('state_{}.pth'.format(optimizer_idx))['state_dict']
+            state_dict = phase.module.state_dict()
+            for k in state_dict_old:
+                v1 = state_dict_old[k]
+                v4 = state_dict[k]
+                if (v1 == v4).all():
+                    print(k)
+
+        if batch_idx == 1:
+            torch.save({'state_dict': phase.module.state_dict()}, 'state_{}.pth'.format(optimizer_idx))
 
     # def on_validation_start(self):
     #     if self.metrics:
