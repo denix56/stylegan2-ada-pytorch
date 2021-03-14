59e64df4ec570570317ad5b8d711d7864c2c2132
dsenkin <denisx9.0c@gmail.com>
2021-03-14 12:50:28 +0100

    fixes

--- training/networks.py
+++ training/networks.py
@@ -163,7 +163,7 @@ class Conv2dLayer(torch.nn.Module):
         w = (self.weight * self.weight_gain)
         b = self.bias.to(dtype=x.dtype) if self.bias is not None else None
         #flip_weight = (self.up == 1) # slightly faster
-        x = F.conv2d(input=x, weight=w, bias=b)
+        x = F.conv2d(input=x, weight=w, bias=None)
         # x = conv2d_resample.conv2d_resample(x=x, w=w,
         #                                     padding=self.padding)
 
