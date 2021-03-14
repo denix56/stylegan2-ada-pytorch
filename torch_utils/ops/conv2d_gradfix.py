772a1c04a2e9dabda6cd78d41931da70f4b8c8c9
dsenkin <denisx9.0c@gmail.com>
2021-03-14 12:17:39 +0100

    fixes

--- torch_utils/ops/conv2d_gradfix.py
+++ torch_utils/ops/conv2d_gradfix.py
@@ -34,6 +34,7 @@ def no_weight_gradients():
 
 def conv2d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
     if _should_use_custom_op(input):
+        print('use')
         return _conv2d_gradfix(transpose=False, weight_shape=weight.shape, stride=stride, padding=padding, output_padding=0, dilation=dilation, groups=groups).apply(input, weight, bias)
     return torch.nn.functional.conv2d(input=input, weight=weight, bias=bias, stride=stride, padding=padding, dilation=dilation, groups=groups)
 
