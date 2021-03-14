0bb8b30fa551ba2f729f1611fda5c5ff8e6e0eb0
dsenkin <denisx9.0c@gmail.com>
2021-03-14 17:09:04 +0100

    fixes

--- training/training_loop.py
+++ training/training_loop.py
@@ -211,7 +211,7 @@ def training_loop(
 
     trainer = pl.Trainer(gpus=num_gpus, accelerator='ddp', weights_summary='full', fast_dev_run=10,
                          benchmark=cudnn_benchmark, max_steps=total_kimg//(batch_size*num_gpus)*1000,
-                         plugins=DDPPlugin(broadcast_buffers=False))
+                         plugins=DDPPlugin(broadcast_buffers=False, find_unused_parameters=True))
     trainer.fit(net, datamodule=training_set_pl)
 
     # # Distribute across GPUs.
